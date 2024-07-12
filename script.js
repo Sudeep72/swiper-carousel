f2260703604a60cb86abd126d69f71b495566ac2
Sudeep <sudeep7217@gmail.com>
2024-07-12 15:07:47 +0530

    style: update trending section heading to 'C O O K I E S '24'

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var TrandingSlider = new Swiper('.tranding-slider', {
-  effect: 'coverflow',
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   coverflowEffect: {
     rotate: 0,
     stretch: 0,
@@ -15,7 +15,7 @@ var TrandingSlider = new Swiper('.tranding-slider', {
     // clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
